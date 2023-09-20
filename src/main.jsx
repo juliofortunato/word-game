1441be67129fe5ba42228b2679c3e1ef5cdb3829
Júlio Fortunato <juliofortunato58@gmail.com>
2023-09-20 15:36:15 -0300

    chore: update file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import App from './components/App';
+
+import './reset.css';
+import './styles.css';
+
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
