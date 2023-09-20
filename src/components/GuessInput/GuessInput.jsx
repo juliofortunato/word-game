48ecd1558952c7d91e42f987cfa8cd11fdbbed45
Júlio Fortunato <juliofortunato58@gmail.com>
2023-09-20 16:58:11 -0300

    fix: use pattern on GuessInput validation

--- src/components/GuessInput/GuessInput.jsx
+++ src/components/GuessInput/GuessInput.jsx
@@ -15,11 +15,14 @@ function GuessInput() {
       <label htmlFor="guess-input">
         Enter guess:
         <input
+          required
           id="guess-input"
           type="text"
           value={guess}
           minLength={5}
           maxLength={5}
+          pattern="[a-zA-Z]{5}"
+          title="5 letter word"
           onChange={(e) => setGuess(e.target.value.toUpperCase())}
         />
       </label>
