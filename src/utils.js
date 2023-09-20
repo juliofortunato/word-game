0ee7b5c95cb9e6f3a21e919ecd30facdb0cbef67
Júlio Fortunato <juliofortunato58@gmail.com>
2023-09-20 15:44:48 -0300

    feat: add utility files

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+export const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+export const range = (start, end, step = 1) => {
+  const output = [];
+
+  if (typeof end === 'undefined') {
+    end = start;
+    start = 0;
+  }
+
+  for (let i = start; i < end; i += step) {
+    output.push(i);
+  }
+
+  return output;
+};
